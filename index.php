e4fd23438c1c0a61b9c1e387563f161794065409
bhavy20110 <asmitapatel2340@gmail.com>
2025-03-11 15:38:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,15 @@ header("Content-Type: application/vnd.apple.mpegurl");
 $d0 = isset($_GET["id"]) ? $_GET["id"] : "";
 $j1 = isset($_GET["au"]) ? $_GET["au"] : "";
 
-$n2 = "http://tatatv.cc/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http://localhost/ch/{$d0}&series=&forced_storage=0&disable_ad=0&download=0&force_ch_link_check=0&JsHttpRequest=1-xml";
+$n2 = "http://atg100.xyz/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http://localhost/ch/{$d0}&series=&forced_storage=0&disable_ad=0&download=0&force_ch_link_check=0&JsHttpRequest=1-xml";
 
 $t4 = array(
-    "Cookie: mac=00:1A:79:e0:68:9c",
-    "Authorization: Bearer 2AF105C4CADD3071E59B5AEFDC39659A",
-    "Referer: http://tatatv.cc/stalker_portal/c/",
+    "Cookie: mac=00:1A:79:AD:F2:91",
+    "Authorization: Bearer 4B879C95D6C82342C317E25030AA543F",
+    "Referer: http://atg100.xyz/stalker_portal/c/",
     "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
     "X-User-Agent: Model: MAG250; Link:",
-    "Referer: http://tatatv.cc/stalker_portal/c/",
-    "X-Device-ID: CF53E643CDF8E4FACE18B68F36A204D8A8BBFC2C7C641A25DA6D9C8DA29E368F",
-    "X-Device-ID-2: 2AB3EAFBD4029"
+    "Referer: http://atg100.xyz/stalker_portal/c/"
 );
 
 $n3 = curl_init();
@@ -24,7 +22,7 @@ curl_setopt($n3, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($n3, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($n3, CURLOPT_HTTPHEADER, $t4);
 curl_setopt($n3, CURLOPT_USERAGENT, 'Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3');
-curl_setopt($n3, CURLOPT_REFERER, 'http://tatatv.cc/stalker_portal/c/');
+curl_setopt($n3, CURLOPT_REFERER, 'http://atg100.xyz/stalker_portal/c/');
 
 $h5 = curl_exec($n3);
 
