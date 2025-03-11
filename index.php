fcc48e5894547a3381eda900f483b25b8f3b3c8d
bhavy20110 <asmitapatel2340@gmail.com>
2025-03-11 15:16:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,17 @@ header("Content-Type: application/vnd.apple.mpegurl");
 $d0 = isset($_GET["id"]) ? $_GET["id"] : "";
 $j1 = isset($_GET["au"]) ? $_GET["au"] : "";
 
-// Replace with actual panel URL
 $n2 = "http://tatatv.cc/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http://localhost/ch/{$d0}&series=&forced_storage=0&disable_ad=0&download=0&force_ch_link_check=0&JsHttpRequest=1-xml";
 
 $t4 = array(
-    "Cookie: mac=00:1A:79:e0:68:9c",  // Your MAC address
-    "Authorization: Bearer 2AF105C4CADD3071E59B5AEFDC39659A",  // Your Bearer Token
+    "Cookie: mac=00:1A:79:e0:68:9c",
+    "Authorization: Bearer 2AF105C4CADD3071E59B5AEFDC39659A",
     "Referer: http://tatatv.cc/stalker_portal/c/",
     "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
     "X-User-Agent: Model: MAG250; Link:",
     "Referer: http://tatatv.cc/stalker_portal/c/",
-    "X-Device-ID: CF53E643CDF8E4FACE18B68F36A204D8A8BBFC2C7C641A25DA6D9C8DA29E368F",  // Device ID 1
-    "X-Device-ID-2: 2AB3EAFBD4029"  // Device ID 2 (SN)
+    "X-Device-ID: CF53E643CDF8E4FACE18B68F36A204D8A8BBFC2C7C641A25DA6D9C8DA29E368F",
+    "X-Device-ID-2: 2AB3EAFBD4029"
 );
 
 $n3 = curl_init();
